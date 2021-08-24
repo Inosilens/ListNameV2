c064647dcf34984114eca545bf3e2bbdac2f659a
Пользователь <pashin.andrey94@gmail.com>
2021-08-24 13:17:35 +0700

    add polyfill for IE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import "react-app-polyfill/ie9"
+import "react-app-polyfill/ie11"
+import "react-app-polyfill/stable"
+import 'raf/polyfill';
 import React from "react";
 import App from "./App";
 import ReactDOM from "react-dom";
